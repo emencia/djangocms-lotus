98ec77589c2bc74814215bb05bc4e96a9d580e06
David THENON <sveetch@gmail.com>
2025-01-14 16:03:40 +0000

    [CHG] Do no enable debug_toolbar in settings if it is not installed

--- sandbox/settings/demo.py
+++ sandbox/settings/demo.py
@@ -19,32 +19,39 @@ DATABASES = {
 
 """
 django-debug-toolbar part
+
+This is only applied if package has been installed.
 """
-ROOT_URLCONF = "sandbox.localurls"
+try:
+    import debug_toolbar
+except ImportError:
+    pass
+else:
+    ROOT_URLCONF = "sandbox.localurls"
 
-INTERNAL_IPS = ["192.168.0.115"]
+    INTERNAL_IPS = ["192.168.0.115"]
 
-DEBUG_TOOLBAR_PANELS = [
-    # "debug_toolbar.panels.history.HistoryPanel",
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    # "debug_toolbar.panels.signals.SignalsPanel",
-    # "debug_toolbar.panels.redirects.RedirectsPanel",
-    # "debug_toolbar.panels.profiling.ProfilingPanel",
-]
+    DEBUG_TOOLBAR_PANELS = [
+        # "debug_toolbar.panels.history.HistoryPanel",
+        "debug_toolbar.panels.versions.VersionsPanel",
+        "debug_toolbar.panels.timer.TimerPanel",
+        "debug_toolbar.panels.settings.SettingsPanel",
+        "debug_toolbar.panels.headers.HeadersPanel",
+        "debug_toolbar.panels.request.RequestPanel",
+        "debug_toolbar.panels.sql.SQLPanel",
+        "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+        "debug_toolbar.panels.templates.TemplatesPanel",
+        "debug_toolbar.panels.cache.CachePanel",
+        # "debug_toolbar.panels.signals.SignalsPanel",
+        # "debug_toolbar.panels.redirects.RedirectsPanel",
+        # "debug_toolbar.panels.profiling.ProfilingPanel",
+    ]
 
-MIDDLEWARE[0:0] = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-]
+    MIDDLEWARE[0:0] = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ]
 
-INSTALLED_APPS.append("debug_toolbar")
+    INSTALLED_APPS.append("debug_toolbar")
 
 
 # Import local settings if any
